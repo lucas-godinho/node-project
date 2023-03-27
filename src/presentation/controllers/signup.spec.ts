6496de068252d7ddcbc9796ec818f39877c86fe5
Lucas Godinho <lucker.de.souza@gmail.com>
2023-03-27 14:02:05 -0300

    refactor: add customized error for missign param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from "./signup";
+import { MissignParamError } from "../erros/missignParamError";
 
 describe("SignUp Controller", () => {
   test("Should return 400 if no name is provided", () => {
@@ -12,7 +13,7 @@ describe("SignUp Controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missign param: name"));
+    expect(httpResponse.body).toEqual(new MissignParamError("name"));
   });
 
   test("Should return 400 if no email is provided", () => {
@@ -26,6 +27,6 @@ describe("SignUp Controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missign param: email"));
+    expect(httpResponse.body).toEqual(new MissignParamError("email"));
   });
 });
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,23 @@
+import type * as Http from "../protocols/http";
+import { MissignParamError } from "../erros/missignParamError";
+
 export class SignUpController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: Http.Request): Http.Response {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error("Missign param: name"),
+        body: new MissignParamError("name"),
       };
     }
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error("Missign param: email"),
+        body: new MissignParamError("email"),
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: "any_body",
       };
     }
   }
